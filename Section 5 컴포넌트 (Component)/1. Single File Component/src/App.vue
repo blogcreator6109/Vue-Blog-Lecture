0354e9c0beb9cd9ea91985ec9be88732f3414f55
blogcreator6109 <blogcreator6109@gmail.com>
2023-09-27 17:52:10 +0900

    Single File Component

---
+++ Section 5 컴포넌트 (Component)/1. Single File Component/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2>{{ text }}</h2>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    return {
+      text: ref("Hello"),
+    };
+  },
+};
+</script>
+
+<style>
+h2 {
+  color: red;
+}
+</style>
---
+++ Section 5 컴포넌트 (Component)/1. Single File Component/과제 답/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <SiteHeader />
+  <SiteMain />
+  <SiteFooter />
+</template>
+
+<script>
+import { ref } from "vue";
+import SiteHeader from "./components/SiteHeader.vue";
+import SiteMain from "./components/SiteMain.vue";
+import SiteFooter from "./components/SiteFooter.vue";
+
+export default {
+  setup() {
+    return {
+      text: ref("Hello"),
+    };
+  },
+  components: { SiteHeader, SiteMain, SiteFooter },
+};
+</script>
+
+<style>
+h2 {
+  color: red;
+}
+</style>
