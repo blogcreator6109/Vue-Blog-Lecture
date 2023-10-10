fe541ed3534ff4026449d4b490e28b076af2ec49
blogcreator6109 <blogcreator6109@gmail.com>
2023-10-11 07:51:36 +0900

    5-2까지 구현

---
+++ Section 5 컴포넌트 (Component)/2. Vue 프로젝트 생성하기/과제답/src/components/SiteHeader.vue
@@ -0,0 +1,48 @@
+<template>
+  <header>
+    <img src="@/assets/logo.jpg" />
+    <nav>
+      <a href="" v-for="item of menu" :key="item" class="menu">
+        {{ item }}
+      </a>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {
+      menu: ["HTML", "CSS", "Javascript"],
+    };
+  },
+};
+</script>
+
+<style>
+header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+img {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+}
+
+nav {
+  margin-top: 20px;
+  width: 80%;
+  background-color: #333;
+  padding: 5px 10px;
+  border-radius: 5px;
+}
+
+a {
+  margin-right: 10px;
+  font-weight: bold;
+  text-decoration: none;
+  color: white;
+}
+</style>
